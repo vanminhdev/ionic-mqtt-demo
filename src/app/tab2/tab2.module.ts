ce6fcb08b7582186affe8597a80b2840fdbcc0e2
Lê Văn Minh <vanminh.dev@gmail.com>
2019-11-11 08:22:47 +0700

    lam xong connect mqtt

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,17 +1,26 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { IonicMqttModule, MQTTService } from 'ionic-mqtt';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([{ path: '', component: Tab2Page }]),
+    IonicMqttModule
+  ],
+  providers: [
+    MQTTService
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+
+
+export class Tab2PageModule {
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
+import { MQTTService } from 'ionic-mqtt';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
 
-  constructor() {}
+  private _mqttClient: any;
 
+  private MQTT_CONFIG: {
+    host: string,
+    port: number,
+    clientId: string,
+  } = {
+    host: "broker.mqttdashboard.com",
+    port: 8000,
+    clientId: "clientId-LtNoLonsYY",
+  };
+
+  private TOPIC: string[] = ["topictest"];
+
+  constructor(private _mqttService: MQTTService) {
+  }
+
+  ngOnInit() {
+    this._mqttClient = this._mqttService.loadingMqtt(this._onConnectionLost, this._onMessageArrived, this.TOPIC, this.MQTT_CONFIG);
+  }
+
+  private _onConnectionLost(responseObject) {
+    // connection listener
+    // ...do actions when connection lost
+    console.log('_onConnectionLost ', responseObject);
+  }
+
+  private _onMessageArrived(message) {
+    // message listener
+    // ...do actions with arriving message
+    console.log('message arrived ', message.payloadString);
+  }
+
+  // public function for sending and publishing mqtt messages
+
+  public sendMessage() {
+    console.log('sendMessage')
+    this._mqttService.sendMessage("topictest", "loi nhan");
+  }
+
+  public publishMessage() {
+    console.log('publishMessage')
+    this._mqttService.publishMessage("toppictest", "loi nhan");
+  }
+
+
+  private messageArrived: string;
+
+  //
+  getData() {
+
+    // alert('load data');
+    console.log('chay function get data');
+    
+    this.sendMessage();
+
+
+
+  }
 }
